6e4c26847a308f996d60111781800ec297344ac4
daanish <daanish222@gmail.com>
2025-07-02 21:14:24 +0530

    02-07-2025

--- initial_basic_template.cpp
+++ initial_basic_template.cpp
@@ -1,24 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-using i64=long long;
-
-void solve(){
-    
-
-
+using i64 = long long;
+using u64 = unsigned long long;
+using u32 = unsigned;
+ 
+// using u128 = unsigned__int128;
+// using i128 = __int128;
+ 
+void solve() {
+   
 }
-
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int tt;
-    cin >> tt;
-
-    while(tt--){
+ 
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int t;
+    std::cin >> t;
+    
+    while (t--) {
         solve();
     }
-
+    
     return 0;
 }
